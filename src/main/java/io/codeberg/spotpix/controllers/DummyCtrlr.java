7c94566147d3f536e65b48e0149d10a5394bad33
Mohammad Issawi <2000issawi@gmail.com>
2023-05-12 01:34:55 +0300

    Add indexed image data structure and convert from indexed to byte image

--- src/main/java/io/codeberg/spotpix/controllers/DummyCtrlr.java
+++ src/main/java/io/codeberg/spotpix/controllers/DummyCtrlr.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import io.codeberg.spotpix.model.Color;
+import io.codeberg.spotpix.model.ColorSpace;
 import io.codeberg.spotpix.model.decoders.JDecoder;
-import io.codeberg.spotpix.model.images.Image;  
+import io.codeberg.spotpix.model.images.Image;
+import io.codeberg.spotpix.model.images.IndexedImage;  
 
 public class DummyCtrlr {
     public BufferedImage getImage(){
@@ -22,4 +25,25 @@ public class DummyCtrlr {
         Image img=(new JDecoder()).decode(bytes);
         return img.toBufferedImage();
     }   
+    public BufferedImage getIndexedImage(){
+        Color black=new Color(0xFF000000);
+        Color white=new Color(0xFFFFFF);
+        Color[] colorMap=new Color[2];
+        colorMap[0]=black;
+        colorMap[1]=white;
+
+        int[][] indices=new int[100][100];
+        for(int i=0;i<100;i++){
+            for(int j=0;j<100;j++){
+                if(i%2==0){
+                    indices[i][j]=0;
+                }else{
+                    indices[i][j]=1;
+                }
+            }
+        }
+
+        return (new IndexedImage(colorMap, indices, 100, 100,ColorSpace.sRGB)).toBufferedImage();
+
+    }
 }
--- src/main/java/io/codeberg/spotpix/model/images/IndexedImage.java
+++ src/main/java/io/codeberg/spotpix/model/images/IndexedImage.java
@@ -2,12 +2,41 @@ package io.codeberg.spotpix.model.images;
 
 import java.awt.image.BufferedImage;
 
-public class IndexedImage implements Image {
+import io.codeberg.spotpix.model.Color;
+import io.codeberg.spotpix.model.ColorSpace;
 
+public class IndexedImage implements Image {
+    private Color[] colorMap;
+    private int[][] indices;
+    private int height,width;
+    private ColorSpace colorSpace;
+    public IndexedImage(Color[] colorMap,int[][] indices,int height,int width,ColorSpace colorSpace){
+        this.colorMap=colorMap;
+        this.indices=indices;
+        this.height=height;
+        this.width=width;
+        this.colorSpace=colorSpace;
+    }
+    public IndexedImage(Color[] colorMap,int[][] indices,int height,int width){
+        this.colorMap=colorMap;
+        this.indices=indices;
+        this.height=height;
+        this.width=width;
+        this.colorSpace=ColorSpace.LINEAR;
+    }
+
+    public ByteImage toByteImage(){
+        Color[][] colors=new Color[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                colors[i][j] = colorMap[indices[i][j]];
+            }
+        }
+        return new ByteImage(colors, height, width,colorSpace);
+    }
     @Override
     public BufferedImage toBufferedImage() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toBufferedImage'");
+        return toByteImage().toBufferedImage();
     }
     
 }
