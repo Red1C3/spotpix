1ad95088173064b143e72262a42163eb9a3a1c26
Mohammad Issawi <2000issawi@gmail.com>
2023-05-12 22:03:56 +0300

    Implement RGB to XYZ

--- src/main/java/io/codeberg/spotpix/model/Color.java
+++ src/main/java/io/codeberg/spotpix/model/Color.java
@@ -2,10 +2,12 @@ package io.codeberg.spotpix.model;
 
 public class Color {
     private int argb;
+    private double[] xyz;
 
     // TODO LABS fmt
     public Color(int argb) {
         this.argb = argb;
+        this.xyz=RGB2XYZ(getRed(), getGreen(), getBlue());
         // TODO calculate other formats values
     }
 
@@ -15,6 +17,7 @@ public class Color {
         argb|=((r<<16)&0xFF0000);
         argb|=((g<<8)&0xFF00);
         argb|=(b&0xFF);
+        this.xyz=RGB2XYZ(r, g, b);
     }
 
     public int getARGB() {
@@ -51,5 +54,40 @@ public class Color {
         int blue = Math.max(color0.getBlue() + color1.getBlue(), 255);
 
         return new Color(alpha, red, green, blue);
+    }
+
+    public static double[] RGB2XYZ(int ir,int ig,int ib){
+        double r=((double)ir)/255.0;
+        double g=((double)ig)/255.0;
+        double b=((double)ib)/255.0;
+
+        if(r>0.04045){
+            r=Math.pow(((r+0.055)/1.055), 2.4);
+        }else{
+            r=r/12.92;
+        }
+
+        if(g>0.04045){
+            g=Math.pow(((g+0.055)/1.055),2.4);
+        }else{
+            g=g/12.92;
+        }
+
+        if(b>0.04045){
+            b=Math.pow((b+0.055)/1.055,2.4);
+        }else{
+            b=b/12.92;
+        }
+
+        r=r*100;
+        g=g*100;
+        b=b*100;
+
+        double[] xyz=new double[3];
+        xyz[0]=r*0.4124+g*0.3576+b*0.1805;
+        xyz[1]=r*0.2126+g*0.7152+b*0.0722;
+        xyz[2]=r*0.0193+b*0.1192+b*0.9505;
+
+        return xyz;
     }
 }
