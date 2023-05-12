b3d7be75c25407b852d3cb70b511fec51ce1a656
Mohammad Issawi <2000issawi@gmail.com>
2023-05-12 22:11:27 +0300

    Update dummies

--- src/main/java/io/codeberg/spotpix/controllers/DummyCtrlr.java
+++ src/main/java/io/codeberg/spotpix/controllers/DummyCtrlr.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import io.codeberg.spotpix.model.Color;
 import io.codeberg.spotpix.model.ColorSpace;
 import io.codeberg.spotpix.model.Pixel;
+import io.codeberg.spotpix.model.comparators.RGBComparator;
 import io.codeberg.spotpix.model.decoders.JDecoder;
 import io.codeberg.spotpix.model.images.Image;
 import io.codeberg.spotpix.model.images.IndexedImage;
@@ -25,7 +26,7 @@ public class DummyCtrlr {
             return new BufferedImage(0, 0, BufferedImage.TYPE_3BYTE_BGR);
         }
         Image img = (new JDecoder()).decode(bytes);
-        return img.toIndexedImage().toBufferedImage();
+        return img.toBufferedImage();
     }
 
     public BufferedImage getIndexedImage() {
@@ -36,15 +37,17 @@ public class DummyCtrlr {
         colorMap.add(white);
 
         int[][] indices = new int[100][100];
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (i % 2 == 0) {
-                    indices[i][j] = 0;
-                } else {
-                    indices[i][j] = 1;
-                }
-            }
-        }
+        indices[0][0]=1;
+        indices[2][2]=1;
+        // for (int i = 0; i < 100; i++) {
+        //     for (int j = 0; j < 100; j++) {
+        //         if (i % 2 == 0) {
+        //             indices[i][j] = 0;
+        //         } else {
+        //             indices[i][j] = 1;
+        //         }
+        //     }
+        // }
 
         IndexedImage img = (new IndexedImage(colorMap, indices, 100, 100, ColorSpace.sRGB));
 
@@ -54,7 +57,11 @@ public class DummyCtrlr {
                 img.setPixel(new Pixel(red, i, j));
             }
         }
-        
+        Pixel[] region=img.getRegion(img.getPixel(0, 0), new RGBComparator(),null);
+        for (Pixel pixel : region) {
+            pixel.setColor(white);
+        }
+        img.setRegion(region);
         return img.toBufferedImage();
     }
 
