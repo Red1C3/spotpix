8a89f715b8e3c97d50ae8983a48acbcf16c2fc01
Maias-Alakramy <maias-alakramy01@hotmail.com>
2023-05-13 18:25:21 +0300

    it's time to know time

--- src/main/java/io/codeberg/spotpix/App.java
+++ src/main/java/io/codeberg/spotpix/App.java
@@ -6,6 +6,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        long startTime = System.nanoTime();
         new DummyView();
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println((double) totalTime/1.0e9);
     }
 }
