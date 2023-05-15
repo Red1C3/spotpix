aa39c9b1a0cec87a6d7ed14e68732cf343475abd
Maias-Alakramy <maias-alakramy01@hotmail.com>
2023-05-15 17:48:39 +0300

    calcDistance function moved to quantizer interface

--- src/main/java/io/codeberg/spotpix/model/quantizers/Quantizer.java
+++ src/main/java/io/codeberg/spotpix/model/quantizers/Quantizer.java
@@ -1,9 +1,11 @@
 package io.codeberg.spotpix.model.quantizers;
 
+import io.codeberg.spotpix.model.Color;
 import io.codeberg.spotpix.model.colorOps.ColorOp;
 import io.codeberg.spotpix.model.comparators.EqComparator;
 import io.codeberg.spotpix.model.images.Image;
 
 public interface Quantizer {
     Image quantize(Image image,EqComparator compartor,ColorOp colorOp);
+    double calcDistance(Color c1, Color c2);
 }
