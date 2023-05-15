6e21b0341044c6343b4498c93289d6fe35525e04
Mohammad Issawi <2000issawi@gmail.com>
2023-05-15 20:44:12 +0300

    Create viewer root

--- src/main/java/io/codeberg/spotpix/App.java
+++ src/main/java/io/codeberg/spotpix/App.java
@@ -1,13 +1,14 @@
 package io.codeberg.spotpix;
 
 import io.codeberg.spotpix.views.DummyView;
+import io.codeberg.spotpix.views.ViewerRoot;
 
 public class App 
 {
     public static void main( String[] args )
     {
         long startTime = System.nanoTime();
-        new DummyView();
+        new ViewerRoot();
         long endTime   = System.nanoTime();
         long totalTime = endTime - startTime;
         System.out.println((double) totalTime/1.0e9);
---
+++ src/main/java/io/codeberg/spotpix/views/ViewerRoot.java
@@ -0,0 +1,16 @@
+package io.codeberg.spotpix.views;
+
+import javax.swing.JFrame;
+
+public class ViewerRoot extends JFrame {
+    private final static int INITIAL_WIDTH = 640;
+    private final static int INITAIL_HEIGHT = 480;
+    private final static String TITLE = "Spotpix";
+
+    public ViewerRoot() {
+        super(TITLE);
+        setSize(INITIAL_WIDTH, INITAIL_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
