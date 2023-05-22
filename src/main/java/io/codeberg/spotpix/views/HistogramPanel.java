bf8818961491787d8135b05e565a795082bb45e1
Mohammad Issawi <2000issawi@gmail.com>
2023-05-22 17:46:31 +0300

    Another failed histogram

--- src/main/java/io/codeberg/spotpix/views/HistogramPanel.java
+++ src/main/java/io/codeberg/spotpix/views/HistogramPanel.java
@@ -3,10 +3,11 @@ package io.codeberg.spotpix.views;
 import java.util.ArrayList;
 import java.awt.Graphics;
 
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.statistics.SimpleHistogramBin;
 import org.jfree.data.statistics.SimpleHistogramDataset;
 
 import io.codeberg.spotpix.model.Color;
@@ -14,20 +15,29 @@ import io.codeberg.spotpix.model.Color;
 public class HistogramPanel extends ChartPanel {
     private static HistogramPanel histogramPanel;
     private JFreeChart chart;
-    public static HistogramPanel instance(){
-        if(histogramPanel==null) histogramPanel=new HistogramPanel(null);
+
+    public static HistogramPanel instance() {
+        if (histogramPanel == null)
+            histogramPanel = new HistogramPanel(null);
         return histogramPanel;
     }
-    private HistogramPanel(JFreeChart chart){
+
+    private HistogramPanel(JFreeChart chart) {
         super(chart);
     }
-    public void createHistogram(int[] quantizationMap,ArrayList<Color> colorMap){
-        SimpleHistogramDataset dataset=new SimpleHistogramDataset(null);
 
-        for (int quantization : quantizationMap) {
-            dataset.addObservation(quantization);
+    public void createHistogram(int[] quantizationMap, ArrayList<Color> colorMap) {
+        HistogramDataset dataset = new HistogramDataset();
+        int size=0;
+        for(int i=0;i<quantizationMap.length;i++){
+            size+=quantizationMap[i];
         }
-        chart=ChartFactory.createHistogram("Color Histogram", "Colors", "Occurances", dataset);
+        double[] series=new double[size];
+
+        //dataset.addSeries("key", series, series.length,0,4);
+
+        chart = ChartFactory.createHistogram("Color Histogram", "Colors", "Occurances", dataset);
+        setChart(chart);
         repaint();
     }
 }
