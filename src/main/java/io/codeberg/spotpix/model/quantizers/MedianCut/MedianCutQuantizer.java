9df0495d83152798678fd534ffb55093ae8bb41f
Mohammad Issawi <2000issawi@gmail.com>
2023-05-22 22:46:09 +0300

    Send quantization map after median cut processing

--- src/main/java/io/codeberg/spotpix/model/quantizers/MedianCut/MedianCutQuantizer.java
+++ src/main/java/io/codeberg/spotpix/model/quantizers/MedianCut/MedianCutQuantizer.java
@@ -39,29 +39,28 @@ public abstract class MedianCutQuantizer implements Quantizer {
         int[][] indices = new int[width][height];
         int[] quantizationMap = new int[colorMap.size()];
 
-
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 Pixel pixel = image.getPixel(i, j);
                 int x = pixel.getX();
                 int y = pixel.getY();
 
-                int index = Distances.getClosestColorId(pixel,colorMap,this);
+                int index = Distances.getClosestColorId(pixel, colorMap, this);
                 indices[x][y] = index;
                 quantizationMap[index] += 1;
             }
         }
 
-        IndexedImage indexedImage = new IndexedImage(colorMap, indices, height, width);
+        IndexedImage indexedImage = new IndexedImage(colorMap, indices, height, width, quantizationMap);
         return indexedImage;
     }
-    
+
     protected void medianCutQuantizer(ArrayList<KColor> colors) {
         int sumR = 0;
         int sumG = 0;
         int sumB = 0;
         int sumA = 0;
-        
+
         for (KColor color : colors) {
             sumR += color.getRed();
             sumG += color.getGreen();
@@ -69,15 +68,15 @@ public abstract class MedianCutQuantizer implements Quantizer {
             sumA += color.getAlpha();
             color.clusterIndex = colorMap.size();
         }
-        
+
         sumR /= colors.size();
         sumG /= colors.size();
         sumB /= colors.size();
         sumA /= colors.size();
-        
+
         colorMap.add(new Color(sumA, sumR, sumG, sumB));
     }
-    
+
     protected abstract void splitIntoBuckets(ArrayList<KColor> colors, int depth);
 
     protected abstract int getRange(ArrayList<KColor> colors);
