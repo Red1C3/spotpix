4e719f4213e2e8eac9a15f596fe971927697dbc4
Mohammad Issawi <2000issawi@gmail.com>
2023-05-30 02:41:49 +0300

    Add search field to search dialog

--- src/main/java/io/codeberg/spotpix/views/SearchDialog.java
+++ src/main/java/io/codeberg/spotpix/views/SearchDialog.java
@@ -1,15 +1,22 @@
 package io.codeberg.spotpix.views;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
-public class SearchDialog extends JDialog{
+public class SearchDialog extends JDialog implements ActionListener{
     private final static String COLOR_SRCH_STR="Color Search";
 
     private JPanel colorPanel;
+    private JTextField pathField;
     public SearchDialog(){
         super(ViewerRoot.instance(),"Search",ModalityType.APPLICATION_MODAL);
 
@@ -18,16 +25,30 @@ public class SearchDialog extends JDialog{
 
         JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 
+        pathField=new JTextField();
+        JButton pathButton=new JButton();
+        pathButton.addActionListener(this);
+        JPanel pathPanel=new JPanel();
+        pathPanel.setLayout(new BorderLayout());
+        pathPanel.add(pathField,BorderLayout.CENTER);
+        pathPanel.add(pathButton,BorderLayout.EAST);
         setupPanels();
 
         tabbedPane.add(colorPanel,COLOR_SRCH_STR);
         add(tabbedPane,BorderLayout.CENTER);
+        add(pathPanel,BorderLayout.NORTH);
         setSize(300, 100);
         setVisible(true);
     }
 
     private void setupPanels(){
         colorPanel=new JPanel();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
     
 }
