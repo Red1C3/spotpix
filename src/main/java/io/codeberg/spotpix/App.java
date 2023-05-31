2d9f98f6f12afe2ef3d7a2ff54e67cf53e9e5390
Mohammad Issawi <2000issawi@gmail.com>
2023-05-31 18:59:50 +0300

    Make viewerroot singletonless (out of charge so commiting)

--- src/main/java/io/codeberg/spotpix/App.java
+++ src/main/java/io/codeberg/spotpix/App.java
@@ -7,12 +7,12 @@ import io.codeberg.spotpix.views.ViewerRoot;
 public class App {
     public static void main(String[] args) {
         if (args.length == 0) {
-            ViewerRoot.instance();
+            new ViewerRoot();
         } else if (args.length == 1) {
             String imgPath = args[0];
-            ViewerRoot.instance();
+            ViewerRoot viewerRoot=new ViewerRoot();
             ImageViewPanel.instance().openImage(imgPath);
-            ViewerRoot.instance().repaint();
+            viewerRoot.repaint();
         }
     }
 }
