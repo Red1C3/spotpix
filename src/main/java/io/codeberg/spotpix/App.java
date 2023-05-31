54aca616526328986b63ae9cd64fa1bcf877c675
Mohammad Issawi <2000issawi@gmail.com>
2023-05-31 18:58:02 +0300

    Take image path as an argument for the process

--- src/main/java/io/codeberg/spotpix/App.java
+++ src/main/java/io/codeberg/spotpix/App.java
@@ -1,16 +1,18 @@
 package io.codeberg.spotpix;
 
 import io.codeberg.spotpix.views.DummyView;
+import io.codeberg.spotpix.views.ImageViewPanel;
 import io.codeberg.spotpix.views.ViewerRoot;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        long startTime = System.nanoTime();
-        ViewerRoot.instance();
-        long endTime   = System.nanoTime();
-        long totalTime = endTime - startTime;
-        System.out.println((double) totalTime/1.0e9);
+public class App {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            ViewerRoot.instance();
+        } else if (args.length == 1) {
+            String imgPath = args[0];
+            ViewerRoot.instance();
+            ImageViewPanel.instance().openImage(imgPath);
+            ViewerRoot.instance().repaint();
+        }
     }
 }
