9974f64321d24d74d36633b4effbfe1b2440f504
Mohammad Issawi <2000issawi@gmail.com>
2023-06-11 18:00:45 +0300

    Some tiny stuff (out of charge)

--- src/main/java/io/codeberg/spotpix/views/SearchResultsDialog.java
+++ src/main/java/io/codeberg/spotpix/views/SearchResultsDialog.java
@@ -3,11 +3,13 @@ package io.codeberg.spotpix.views;
 import java.util.ArrayList;
 
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 import io.codeberg.spotpix.model.images.Image;
 
 public class SearchResultsDialog extends JDialog {
     private static final String SEARCH_RES_STR = "Search Results";
+    public static final int imagePanelSize=100;
     private ViewerRoot viewerRoot;
     private ArrayList<Image> images;
 
@@ -15,7 +17,10 @@ public class SearchResultsDialog extends JDialog {
         super(viewerRoot,SEARCH_RES_STR,ModalityType.APPLICATION_MODAL);
         this.viewerRoot=viewerRoot;
         this.images=images;
+        JPanel grid=new JPanel();
+        
 
+        setResizable(false);
         setSize(800,800);
         setVisible(true);
     }
