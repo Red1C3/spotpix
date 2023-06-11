c69ccfe09a183ffcc5e6becfd64f9ce4fbce2d60
Mohammad Issawi <2000issawi@gmail.com>
2023-06-12 02:43:56 +0300

    Open search results selected image in a new windw

--- src/main/java/io/codeberg/spotpix/views/SearchResultsDialog.java
+++ src/main/java/io/codeberg/spotpix/views/SearchResultsDialog.java
@@ -12,7 +12,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import io.codeberg.spotpix.App;
 import io.codeberg.spotpix.model.images.Image;
 
 public class SearchResultsDialog extends JDialog {
@@ -29,7 +32,7 @@ public class SearchResultsDialog extends JDialog {
         grid.setLayout(new GridLayout((int) Math.ceil(images.size() / 6.0f), 6, 5, 5));
 
         for (Image image : images) {
-            grid.add(new ImageItem(viewerRoot.getImageViewPanel(), image));
+            grid.add(new ImageItem(viewerRoot.getImageViewPanel(), image, this));
         }
 
         add(new JScrollPane(grid, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
@@ -38,19 +41,44 @@ public class SearchResultsDialog extends JDialog {
     }
 }
 
-class ImageItem extends JPanel {
+class ImageItem extends JPanel implements MouseListener {
     private ImageViewPanel imageViewPanel;
     private Image image;
+    private SearchResultsDialog searchResultsDialog;
 
-    public ImageItem(ImageViewPanel imageViewPanel, Image image) {
+    public ImageItem(ImageViewPanel imageViewPanel, Image image, SearchResultsDialog searchResultsDialog) {
         this.imageViewPanel = imageViewPanel;
         this.image = image;
+        this.searchResultsDialog = searchResultsDialog;
         setBorder(BorderFactory.createDashedBorder(Color.DARK_GRAY));
+        addMouseListener(this);
     }
 
     @Override
     public void paint(Graphics g) {
         imageViewPanel.paint(g, getHeight(), getWidth(), image);
         paintBorder(g);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        searchResultsDialog.dispose();
+        App.main(image);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
