7a4fbd67d0d09983f1d1b8010d8471caa0a1b959
Mohammad Issawi <2000issawi@gmail.com>
2023-06-12 02:19:34 +0300

    Draw a border for each image item

--- src/main/java/io/codeberg/spotpix/views/SearchResultsDialog.java
+++ src/main/java/io/codeberg/spotpix/views/SearchResultsDialog.java
@@ -2,11 +2,13 @@ package io.codeberg.spotpix.views;
 
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.text.html.ImageView;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
@@ -24,16 +26,14 @@ public class SearchResultsDialog extends JDialog {
         this.viewerRoot = viewerRoot;
         this.images = images;
         JPanel grid = new JPanel();
-        grid.setLayout(new GridLayout((int) Math.ceil(images.size() / 8.0f), 8));
+        grid.setLayout(new GridLayout(Math.max((int) Math.floor(images.size() / 7.0f), 7), 7, 5, 5));
 
         for (Image image : images) {
             grid.add(new ImageItem(viewerRoot.getImageViewPanel(), image));
         }
 
-        JScrollPane scrollPane = new JScrollPane(grid, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        add(scrollPane);
-        setSize(800, 800);
+        add(grid);
+        setSize(700, 700);
         setVisible(true);
     }
 }
@@ -45,10 +45,12 @@ class ImageItem extends JPanel {
     public ImageItem(ImageViewPanel imageViewPanel, Image image) {
         this.imageViewPanel = imageViewPanel;
         this.image = image;
+        setBorder(BorderFactory.createDashedBorder(Color.DARK_GRAY));
     }
 
     @Override
     public void paint(Graphics g) {
         imageViewPanel.paint(g, getHeight(), getWidth(), image);
+        paintBorder(g);
     }
 }
